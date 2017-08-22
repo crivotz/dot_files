82a2a6745f25980180069d30000d53a1949ee6ab
Mauro Locatelli <mauro.locatelli@aretes.net>
2017-08-22 09:58:43 +0200

    Colors and modify after Stretch update

--- linux/screenlayout/3monitor.sh
+++ linux/screenlayout/3monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --mode 1280x1024 --pos 2646x0 --rotate normal --output DP1 --mode 1280x1024 --pos 1366x0 --rotate normal --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal
+xrandr --output DP-1 --mode 1280x1024 --pos 1366x0 --rotate normal --output HDMI-1 --mode 1280x1024 --pos 2646x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal
