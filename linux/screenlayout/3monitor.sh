ea0e8b21d2df34cd6270f285392117564309bbc7
Mauro Locatelli <mauro@locatelli.dev>
2019-07-12 14:58:35 +0200

    Cleaned

--- linux/screenlayout/3monitor.sh
+++ linux/screenlayout/3monitor.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 1280x1024 --pos 1366x0 --rotate normal --output HDMI-1 --mode 1280x1024 --pos 2646x0 --rotate normal --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal
+# file=/tmp/3monitor.log
+# echo Running >> $file
+# sleep 4
+xrandr --verbose --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal >> $file 2>&1
+xrandr --verbose --output DP-1 --mode 1280x1024 --pos 1366x0 --rotate normal >> $file 2>&1
+xrandr --verbose --output HDMI-1 --mode 1280x1024 --pos 2646x0 --rotate normal >> $file 2>&1
+# echo Done >> $file
