c41c9d81898d4796819a4874fdb7333fb19dad71
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2019-08-10 12:36:46 +0200

    Better font and various fix

--- linux/screenlayout/3monitor.sh
+++ linux/screenlayout/3monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # file=/tmp/3monitor.log
 # echo Running >> $file
-# sleep 4
+# sleep 8
 xrandr --verbose --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal #>> $file 2>&1
 xrandr --verbose --output DP-1 --mode 1280x1024 --pos 1366x0 --rotate normal #>> $file 2>&1
 xrandr --verbose --output HDMI-1 --mode 1280x1024 --pos 2646x0 --rotate normal #>> $file 2>&1
