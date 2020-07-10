273a4904f0e657eaa9e08934e917a94f7ef0bf9b
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2020-07-10 11:03:51 +0200

    New gem default and new screen layout

--- linux/screenlayout/home.sh
+++ linux/screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --primary --mode 1680x1050 --pos 0x0 --rotate normal --output HDMI-1 --off --output eDP-1 --mode 1366x768 --pos 1680x0 --rotate normal
+xrandr --output DP-1 --off --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1 --mode 1366x768 --pos 2560x0 --rotate normal
