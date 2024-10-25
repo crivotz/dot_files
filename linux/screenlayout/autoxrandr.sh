488731154d975cdcabb538a23d9658d3418f5183
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2024-10-25 22:26:47 +0200

    Fix autorandr

--- linux/screenlayout/autoxrandr.sh
+++ linux/screenlayout/autoxrandr.sh
@@ -3,10 +3,10 @@
 #     xrandr --output DP-1 --off --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1 --mode 1366x768 --pos 2560x0 --rotate normal
 # elif $(xrandr | grep -q "HDMI-3 connected");then
 #     xrandr --output eDP-1 --off --output VGA-1 --primary --mode 1440x900 --pos 0x0 --rotate left --output HDMI-1 --off --output DP-1 --mode 1600x900 --pos 900x0 --rotate normal --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --mode 1600x900 --pos 2500x0 --rotate normal
-if $(xrandr | grep -q "DP-1-1 connected");then
+if $(xrandr | grep -q "DP-1-3 connected");then
     xrandr --output eDP-1 --off --output HDMI-1 --off --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off --output DP-1-1 --mode 1440x900 --pos 0x0 --rotate left --output DP-1-2 --mode 1600x900 --pos 900x0 --rotate normal --output DP-1-3 --mode 1600x900 --pos 2500x0 --rotate normal
-if $(xrandr | grep -q "eDP-1 connected");then
-    xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP-1 --mode 1280x1024 --pos 1366x0 --rotate normal --output HDMI-1 --mode 1280x1024 --pos 2646x0 --rotate normal
+elif $(xrandr | grep -q "HDMI-1 connected");then
+    xrandr --output HDMI-1 --mode 2560x1440 --pos 1286x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output DP-2 --primary --mode 1280x1024 --pos 0x0 --rotate normal --output HDMI-3 --off --output DP-3 --off
 else
-    xrandr --output HDMI-1 --mode 2560x1440 --pos 1280x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output DP-2 --primary --mode 1280x1024 --pos 0x0 --rotate normal --output HDMI-3 --off --output DP-3 --off
+    xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP-1 --mode 1280x1024 --pos 1366x0 --rotate normal --output HDMI-1 --mode 1280x1024 --pos 2646x0 --rotate normal
 fi
