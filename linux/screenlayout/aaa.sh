5c9d0bb506e6fa25730c23deeec4fd6f63c24060
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2024-10-28 09:16:06 +0100

    Clean

--- linux/screenlayout/aaa.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-1 --mode 2560x1440 --pos 1286x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output DP-2 --primary --mode 1280x1024 --pos 0x0 --rotate normal --output HDMI-3 --off --output DP-3 --off
